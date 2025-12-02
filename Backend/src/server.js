c8797738ef1eed309678476f179bcd34d917125e
Lucky singh <singh.lucky.tect11.gmail.com>
2025-12-02 19:11:00 +0530

    prepared for the deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,41 +1,44 @@
-import express, { response } from "express";
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
-import routes from './Routers/notesRoutes.js'
+import routes from "./Routers/notesRoutes.js";
 import { ConnectDataBase } from "./Config/Db.js";
 import rateLimiter from "./MiddleWare/RateLimiter.js";
 
-
-dotenv.config()
-
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
+if (process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: "http://localhost:5173",
+        })
+    );
+}
 
-app.use(cors({
-    origin: "http://localhost:5173"
-}));
-
-//Middleware 
-app.use(express.json()); // This middleware will parse JSON bodies: req.body
+app.use(express.json());
 app.use(rateLimiter);
-
-// simple costum middleware
-// app.use((req,response,next)=>{
-//     console.log(`res method is ${req.method ? req.method : "not found"} & res URL is ${req.url}`);
-//     next();
-// })
-
 
 app.use("/api/notes", routes);
 
+// -------------------------------
+// PRODUCTION SETUP FIXED
+// -------------------------------
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+    });
+}
 
 ConnectDataBase().then(() => {
     app.listen(PORT, () => {
         console.log(`Server started on : ${PORT}`);
-    })
-})
-
-
+    });
+});
--- Frontend/src/LIbe/Axios.js
+++ Frontend/src/LIbe/Axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+// in Production, there's no localhost so we have to make this dynamic
+const BaseURL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: BaseURL,
 })
 
 export default api;
